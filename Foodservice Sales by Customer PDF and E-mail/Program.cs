b37aef8bc5e3fdf43436a050af8f75065f6931e3
Darron Absher <d.absher@teammodern.com>
2019-01-11 09:28:10 -0500

    Initial Commit.

--- Foodservice Sales by Customer PDF and E-mail/Program.cs
+++ Foodservice Sales by Customer PDF and E-mail/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ModernCR;
 
 namespace Foodservice_Sales_by_Customer_PDF_and_E_mail
 {
@@ -10,6 +6,22 @@ namespace Foodservice_Sales_by_Customer_PDF_and_E_mail
     {
         static void Main(string[] args)
         {
+            var cr = new RunCrystal();
+
+            cr.BuildReport("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Auto-Run.rpt", "D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer.pdf");
+            cr.BuildReport("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice Auto-Run.rpt", "D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice.pdf");
+            cr.BuildReport("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar Auto-Run.rpt", "D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar.pdf");
+
+            cr.SetSubject("Profit by Customer");
+            cr.SetToAddress("b.harris@teammodern.com");
+            cr.SetToAddress("j.shearer@teammodern.com");
+            cr.SetToAddress("k.bishop@teammodern.com");
+            cr.SetToAddress("j.shackleford@teammodern.com");
+            cr.SetToAddress("d.absher@teammodern.com");
+            cr.SetAttachments("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer.pdf");
+            cr.SetAttachments("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice.pdf");
+            cr.SetAttachments("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar.pdf");
+            cr.SendEmail();
         }
     }
 }
