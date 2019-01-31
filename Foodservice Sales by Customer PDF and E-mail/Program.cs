4942678924deb5808fc4bae3aa06cf3c2b06c383
Darron Absher <d.absher@teammodern.com>
2019-01-31 13:39:35 -0500

    Removed Old Reports.

--- Foodservice Sales by Customer PDF and E-mail/Program.cs
+++ Foodservice Sales by Customer PDF and E-mail/Program.cs
@@ -8,10 +8,10 @@ namespace Foodservice_Sales_by_Customer_PDF_and_E_mail
         {
             var cr = new RunCrystal();
 
-            cr.BuildReport("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Auto-Run.rpt", "D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer.pdf");
-            cr.BuildReport("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice Auto-Run.rpt", "D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice.pdf");
+            //cr.BuildReport("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Auto-Run.rpt", "D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer.pdf");
+            //cr.BuildReport("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice Auto-Run.rpt", "D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice.pdf");
             cr.BuildReport("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice Auto-Run Updated.rpt", "D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice Auto-Run Updated.pdf");
-            cr.BuildReport("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar Auto-Run.rpt", "D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar.pdf");
+            //cr.BuildReport("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar Auto-Run.rpt", "D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar.pdf");
             cr.BuildReport("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar Auto-Run Updated.rpt", "D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar Auto-Run Updated.pdf");
 
             cr.SetSubject("Profit by Customer");
@@ -20,10 +20,10 @@ namespace Foodservice_Sales_by_Customer_PDF_and_E_mail
             cr.SetToAddress("k.bishop@teammodern.com");
             cr.SetToAddress("j.shackleford@teammodern.com");
             cr.SetToAddress("d.absher@teammodern.com");
-            cr.SetAttachments("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer.pdf");
-            cr.SetAttachments("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice.pdf");
+            //cr.SetAttachments("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer.pdf");
+            //cr.SetAttachments("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice.pdf");
             cr.SetAttachments("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice Auto-Run Updated.pdf");
-            cr.SetAttachments("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar.pdf");
+            //cr.SetAttachments("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar.pdf");
             cr.SetAttachments("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar Auto-Run Updated.pdf");
             cr.SendEmail();
         }
