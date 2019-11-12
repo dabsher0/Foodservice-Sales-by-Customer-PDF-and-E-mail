11866906e1f5f3455b20120451b768e2689ea61d
Darron Absher <d.absher@teammodern.com>
2019-11-12 10:17:10 -0500

    Added foodservice report for OCA.

--- Foodservice Sales by Customer PDF and E-mail/Program.cs
+++ Foodservice Sales by Customer PDF and E-mail/Program.cs
@@ -13,6 +13,8 @@ namespace Foodservice_Sales_by_Customer_PDF_and_E_mail
             cr.BuildReport("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice Auto-Run Updated.rpt", "D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice Auto-Run Updated.pdf");
             //cr.BuildReport("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar Auto-Run.rpt", "D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar.pdf");
             cr.BuildReport("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar Auto-Run Updated.rpt", "D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar Auto-Run Updated.pdf");
+            cr.BuildReport("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar Auto-Run Updated OCA.rpt", "D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar Auto-Run Updated OCA.pdf");
+            cr.BuildReport("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice Auto-Run Updated OCA.rpt", "D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice Auto-Run Updated OCA.pdf");
 
             cr.SetSubject("Profit by Customer");
             cr.SetToAddress("b.harris@teammodern.com");
@@ -26,6 +28,13 @@ namespace Foodservice_Sales_by_Customer_PDF_and_E_mail
             //cr.SetAttachments("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar.pdf");
             cr.SetAttachments("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar Auto-Run Updated.pdf");
             cr.SendEmail();
+
+            var crOca = new RunCrystal();
+            crOca.SetSubject("Profit by Customer");
+            crOca.SetToAddress("t.mcginnis@teammodern.com");
+            crOca.SetAttachments("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Beverage Bar Auto-Run Updated OCA.pdf");
+            crOca.SetAttachments("D:\\Programs\\Profit by Customer PDF and Email\\Profit by Customer Foodservice Auto-Run Updated OCA.pdf");
+            crOca.SendEmail();
         }
     }
 }
